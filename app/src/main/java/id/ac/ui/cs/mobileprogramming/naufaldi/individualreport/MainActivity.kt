b8dba3f3b143fabb2b8194ee4139d54d51bebe97
Naufaldi Athallah Rifqi <naufaldi.rifqi@akseleran.com>
2020-10-11 14:14:47 +0700

    sharing topik

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/naufaldi/individualreport/MainActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/naufaldi/individualreport/MainActivity.kt
@@ -1,11 +1,43 @@
 package id.ac.ui.cs.mobileprogramming.naufaldi.individualreport
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.drawable.Animatable
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Tween Animation Example (Uncomment code below)
+        val image: ImageView = findViewById(R.id.image)
+        val hyperspaceJump: Animation = AnimationUtils.loadAnimation(this, R.anim.hyperspace_jump)
+        image.startAnimation(hyperspaceJump)
+
+
+        //Frame Animation Example
+        val imageAnimation = image.background
+        if (imageAnimation is Animatable) {
+            imageAnimation.start()
+        }
+
+
+        //Color list Example (Uncomment code below)
+        button.visibility = View.VISIBLE
+
+
+        //Drawable Shapes Example (Uncomment code below)
+        button.setOnClickListener {
+            val intent = Intent(this, ShapesExample::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/naufaldi/individualreport/ShapesExample.kt
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.mobileprogramming.naufaldi.individualreport
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ShapesExample : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.shapes_example_activity)
+    }
+}
