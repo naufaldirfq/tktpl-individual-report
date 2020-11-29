3c365ce8ba3e1de364b9370352948ca452b73ebc
Naufaldi Athallah Rifqi <naufaldi.rifqi@akseleran.com>
2020-11-30 00:16:40 +0700

    Lab 5 fix

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/naufaldi/individualreport/WifiReceiver.java
@@ -0,0 +1,84 @@
+package id.ac.ui.cs.mobileprogramming.naufaldi.individualreport;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class WifiReceiver extends BroadcastReceiver {
+    WifiManager wifiManager;
+    StringBuilder sb;
+    ListView wifiDeviceList;
+
+    public WifiReceiver(WifiManager wifiManager, ListView wifiDeviceList) {
+        this.wifiManager = wifiManager;
+        this.wifiDeviceList = wifiDeviceList;
+    }
+
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if (WifiManager.SCAN_RESULTS_AVAILABLE_ACTION.equals(action)) {
+            sb = new StringBuilder();
+            List<ScanResult> wifiList = wifiManager.getScanResults();
+            final ArrayList<String> deviceList = new ArrayList<>();
+            for (ScanResult scanResult : wifiList) {
+                sb.append("\n").append(scanResult.SSID).append(" - ").append(scanResult.capabilities);
+                deviceList.add(scanResult.SSID + " - " + scanResult.capabilities);
+            }
+            Toast.makeText(context, sb, Toast.LENGTH_SHORT).show();
+            ArrayAdapter arrayAdapter = new ArrayAdapter(context, android.R.layout.simple_list_item_1, deviceList.toArray());
+            wifiDeviceList.setAdapter(arrayAdapter);
+            postData(context, deviceList);
+        }
+
+    }
+
+    private void postData(Context context, ArrayList<String> deviceList) {
+        String url = "https://ad55363ae0eda6024250b30b1fe27e21.m.pipedream.net";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+
+        JSONObject postData = new JSONObject();
+        try {
+            postData.put("wifi", deviceList);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, postData, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("Response>", String.valueOf(response));
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        requestQueue.add(jsonObjectRequest);
+    }
+
+
+}
